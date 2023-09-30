9676816760b9da0ccec91717d71cfde1ded60447
PabloProst <pabloprost16@gmail.com>
2023-09-30 18:12:53 +0200

    Colorpicker shows your color above, line 1 to 32 javascript

---
+++ js/main.js
@@ -0,0 +1,32 @@
+const colorPick1 = document.getElementById('colorPick1')
+const colorCopy1 = document.getElementById('colorCopy1')
+
+colorPick1.addEventListener('input', () => {
+	colorCopy1.style.backgroundColor = colorPick1.value;
+    console.log ("color copiado");
+})
+
+const colorPick2 = document.getElementById('colorPick2')
+const colorCopy2 = document.getElementById('colorCopy2')
+
+colorPick2.addEventListener('input', () => {
+	colorCopy2.style.backgroundColor = colorPick2.value;
+    console.log ("color copiado");
+})
+
+const colorPick3 = document.getElementById('colorPick3')
+const colorCopy3 = document.getElementById('colorCopy3')
+
+colorPick3.addEventListener('input', () => {
+	colorCopy3.style.backgroundColor = colorPick3.value;
+    console.log ("color copiado");
+})
+
+const colorPick4 = document.getElementById('colorPick4')
+const colorCopy4 = document.getElementById('colorCopy4')
+
+colorPick4.addEventListener('input', () => {
+	colorCopy4.style.backgroundColor = colorPick4.value;
+    console.log ("color copiado");
+})
+
