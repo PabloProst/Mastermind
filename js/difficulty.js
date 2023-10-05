8c1225366300e452cea8e596bc353774f68566ac
PabloProst <pabloprost16@gmail.com>
2023-10-05 10:43:36 +0200

    finished easy mode

---
+++ js/difficulty.js
@@ -0,0 +1,7 @@
+const easy = document.getElementById('easy');
+const hard = document.getElementById('hard');
+const impossible = document.getElementById('impossible');
+
+easy.addEventListener('click', () => {
+    window.location.href = "../pages/colorselect.html";
+});
--- js/namepage.js
+++ js/namepage.js
@@ -2,7 +2,7 @@ const saveNameAndRedirect = () => {
     const nickname = document.getElementById('nickname').value
     if (nickname != "") {
         localStorage.setItem('playerName', JSON.stringify(nickname));
-        window.location.href = "../pages/colorselect.html";
+        window.location.href = "../pages/difficulty.html";
     } else {
         alert("You must enter a name.")
     }
